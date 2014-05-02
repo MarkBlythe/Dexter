2c44b745a5cbcaa233ab9ba175fa830c51f2d47c
MarkBlythe <mark@markblythe.co.uk>
2014-05-03 00:01:43 +0100

    Header Fixes
    Title Attribute changed and IE specific stylesheet removed.

--- header.php
+++ header.php
@@ -8,14 +8,11 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title><?php wp_title( '|', true, 'right' ); ?></title>
+<title><?php echo get_bloginfo('name'); ?><?php wp_title(); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <!--[if IE]>
   <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-<![endif]-->
-<!--[if IE 7]>
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/ie.css">
 <![endif]-->
 <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico"/>
 <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
