faae089b8cb9f99041ffc406b860b1505c2e39d4
MarkBlythe <mark@markblythe.co.uk>
2014-05-06 11:52:58 +0100

    Adding Custom Page Template
    Basic custom-template.php for use in custom page layouts.

---
+++ custom-template.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Custom Template
+ * @package Dexter
+ * Use this template for any custom page layouts. Remember to change the Template Name for each.
+ * You can add as much content as you like to this basic example.
+ */
+
+get_header(); ?>
+<section id="content">
+  <div class="inner">
+    <?php while ( have_posts() ) : the_post(); ?>
+      <?php the_content();?>
+    <?php endwhile;?>
+  </div>
+</section>
+<?php get_footer(); ?>
