1b42ed2fe98854872733b6f48f8c988286a16951
MarkBlythe <mark@markblythe.co.uk>
2014-05-10 18:06:25 +0100

    Added Custom Page Template
    This page template can be replicated for any custom pages you require
    in your theme.

--- Dexter/custom-template.php
+++ Dexter/custom-template.php
@@ -5,7 +5,6 @@
  * Use this template for any custom page layouts. Remember to change the Template Name for each.
  * You can add as much content as you like to this basic example.
  */
-
 get_header(); ?>
 <section id="content">
   <div class="inner">
